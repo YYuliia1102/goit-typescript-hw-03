fc82d49f3182316d3a0366f095791ec838ee147f
YYuliia1102 <reya1102@gmail.com>
2023-11-19 22:19:24 +0100

    task complyted

--- src/2.ts
+++ src/2.ts
@@ -15,15 +15,7 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, private department: string, protected salary: number) {
   }
 
   getEmployeeDetails() {
@@ -32,8 +24,13 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000); // Виклик конструктора батьківського класу та збільшення зарплати на 10000
+  }
 }
 
+// Приклад використання класу Manager
+const manager = new Manager('John Doe', 'Management', 50000);
+console.log(manager.getEmployeeDetails()); // Поверне Name: John Doe, Department: Management, Salary: 60000
 
-export {};
+export { };
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+class Key {
+    private isKey: number;
+
+    constructor() {
+        this.isKey = Math.random();
+    }
+
+    getSignature(): number {
+        return this.isKey;
+    }
+}
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Person entered the house.');
+        } else {
+            console.log('The door is closed.');
+        }
+    }
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('The door is opened.');
+        } else {
+            console.log('The door remains closed. Wrong key.');
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +66,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
